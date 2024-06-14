257887d55adf996baf8c656e603410b60fef6a57
Luka <164436043+ghluka@users.noreply.github.com>
2024-06-14 11:32:49 -0400

    Block placed macro check

--- src/main/kotlin/me/ghluka/camel/module/config/pages/DefaultMacroPage.kt
+++ src/main/kotlin/me/ghluka/camel/module/config/pages/DefaultMacroPage.kt
@@ -6,13 +6,15 @@ import cc.polyfrost.oneconfig.events.EventManager
 import cc.polyfrost.oneconfig.events.event.ReceivePacketEvent
 import cc.polyfrost.oneconfig.libs.eventbus.Subscribe
 import cc.polyfrost.oneconfig.utils.dsl.mc
+import net.minecraft.block.Block
 import net.minecraft.init.Blocks
 import net.minecraft.network.play.server.S08PacketPlayerPosLook
 import net.minecraft.util.BlockPos
-import net.minecraft.util.Vec3
 import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.event.world.BlockEvent
 import net.minecraftforge.event.world.WorldEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
 
 class DefaultMacroPage {
     @Exclude
@@ -20,6 +22,8 @@ class DefaultMacroPage {
 
     @Switch(name = "Disable while on bedrock", size = 1)
     var disableOnBedrock: Boolean = true
+    @Switch(name = "Stop on block placed", size = 1)
+    var stopOnBlockPlaced: Boolean = true
     @Switch(name = "Stop on S08PacketPlayerPosLook (TP and rotation)", size = 1)
     var stopOnS08PacketPlayerPosLook: Boolean = true
     @Switch(name = "Stop on world change", size = 1)
@@ -31,8 +35,6 @@ class DefaultMacroPage {
             disableOnBedrock && mc.theWorld.getBlockState(pos.up(1)).block != Blocks.carpet)
             return true
 
-        // TODO: add dirt check
-
         return false
     }
 
@@ -43,15 +45,22 @@ class DefaultMacroPage {
 
     @Subscribe
     fun onPacketReceive(event: ReceivePacketEvent) {
-        if (event.packet is S08PacketPlayerPosLook && stopOnS08PacketPlayerPosLook && mc.thePlayer.posY > 0) {
+        if (event.packet is S08PacketPlayerPosLook && stopOnS08PacketPlayerPosLook && mc.thePlayer.posY > 0)
             emergencyShutOff = true
-        }
     }
 
     @SubscribeEvent
     fun onWorldLoad(event: WorldEvent.Load) {
-        if (stopOnWorldChange) {
+        if (stopOnWorldChange)
             emergencyShutOff = true
-        }
+    }
+
+    @SubscribeEvent
+    fun onBlockPlaced(event: BlockEvent.NeighborNotifyEvent) {
+        val pos = event.pos
+        val block: Block = event.world.getBlockState(pos).block
+
+        if (stopOnBlockPlaced && block.isFullBlock)
+            emergencyShutOff = true
     }
 }

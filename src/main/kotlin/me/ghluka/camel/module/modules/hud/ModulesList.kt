85d378480b6b2ba2c0f3327c81adfebd48b5b008
Luka M <164436043+ghluka@users.noreply.github.com>
2025-08-22 11:56:30 -0400

    more module list options

--- src/main/kotlin/me/ghluka/camel/module/modules/hud/ModulesList.kt
+++ src/main/kotlin/me/ghluka/camel/module/modules/hud/ModulesList.kt
@@ -3,11 +3,15 @@ package me.ghluka.camel.module.modules.hud
 import cc.polyfrost.oneconfig.config.annotations.Dropdown
 import cc.polyfrost.oneconfig.config.annotations.Exclude
 import cc.polyfrost.oneconfig.config.annotations.HUD
+import cc.polyfrost.oneconfig.config.annotations.Page
+import cc.polyfrost.oneconfig.config.annotations.Switch
 import cc.polyfrost.oneconfig.config.core.OneColor
+import cc.polyfrost.oneconfig.config.data.PageLocation
 import cc.polyfrost.oneconfig.hud.TextHud
 import cc.polyfrost.oneconfig.utils.dsl.mc
 import me.ghluka.camel.MainMod
 import me.ghluka.camel.module.Module
+import me.ghluka.camel.module.modules.combat.LeftClicker
 
 class ModulesList : me.ghluka.camel.module.Module(MODULE) {
     @Exclude
@@ -45,15 +49,35 @@ class ModulesList : me.ghluka.camel.module.Module(MODULE) {
         2f,
         OneColor(0, 0, 0)
     ) {
+        @Dropdown(name = "Caps Type", options = ["lower case", "Title Case", "UPPER CASE"])
+        var capsType: Int = 1
+        @Switch(name = "Remove spaces", category = CATEGORY, subcategory = MODULE, size = 1)
+        var removeSpaces = true
+
+        @Exclude
+        val hidden = listOf(
+            "Hilarity"
+        )
+
         override fun getLines(lines: MutableList<String>?, example: Boolean) {
             if (lines == null || mc.thePlayer == null) {
                 return
             }
 
             MainMod.moduleManager.modules.forEach { mod: Module ->
-                if (mod.moduleEnabled && mod.moduleName != "Hilarity")
-                    lines.add(mod.moduleName
-                        .replace(" ", "").replace("-", ""))
+                if (mod.moduleEnabled && !hidden.contains(mod.moduleName)) {
+                    var moduleName = mod.moduleName
+
+                    if (capsType == 0)
+                        moduleName = moduleName.lowercase()
+                    else if (capsType == 2)
+                        moduleName = moduleName.uppercase()
+
+                    if (removeSpaces)
+                        moduleName = moduleName.replace(" ", "").replace("-", "")
+
+                    lines.add(moduleName)
+                }
             }
         }
     }

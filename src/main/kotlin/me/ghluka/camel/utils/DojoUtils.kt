bbefec41345e0e9bc14850ed7666828c0c7bbe22
Luka M <164436043+ghluka@users.noreply.github.com>
2025-08-28 09:44:41 -0400

    dojo bug fix part 2

--- src/main/kotlin/me/ghluka/camel/utils/DojoUtils.kt
+++ src/main/kotlin/me/ghluka/camel/utils/DojoUtils.kt
@@ -4,14 +4,19 @@ import cc.polyfrost.oneconfig.events.EventManager
 import cc.polyfrost.oneconfig.events.event.ReceivePacketEvent
 import cc.polyfrost.oneconfig.libs.eventbus.Subscribe
 import cc.polyfrost.oneconfig.utils.dsl.mc
+import me.ghluka.camel.MainMod
 import net.minecraft.block.BlockStoneBrick
 import net.minecraft.init.Blocks
 import net.minecraft.network.play.server.S08PacketPlayerPosLook
 import net.minecraft.util.BlockPos
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.gameevent.TickEvent
 
 class DojoUtils {
     val defaultSpawn = BlockPos(-207, 100, -598)
     var currentSpawn = defaultSpawn
+
+    var tp: S08PacketPlayerPosLook? = null
 
     init {
         EventManager.INSTANCE.register(this)
@@ -19,20 +24,34 @@ class DojoUtils {
 
     @Subscribe
     fun onPacketReceived(event: ReceivePacketEvent) {
+        // test in singleplayer with /tp -206.5 100 -597.5
         if (event.packet is S08PacketPlayerPosLook && (mc.isSingleplayer || SkyblockUtils.hasLine("Dojo"))) {
-            val tp = event.packet as S08PacketPlayerPosLook
-            // test in singleplayer with /tp -206.5 100 -597.5
-            currentSpawn = getCurrentSpawn(tp)?: defaultSpawn
-            //println("dojo spawn is $currentSpawn, default is $defaultSpawn")
+            tp = event.packet as S08PacketPlayerPosLook
+        }
+    }
+
+    @SubscribeEvent
+    fun onTick(event: TickEvent.ClientTickEvent) {
+        if (mc.thePlayer == null || mc.theWorld == null) return
+        if (event.phase === TickEvent.Phase.START) return
+
+        if (tp != null) {
+            currentSpawn = getCurrentSpawn(tp!!)?: defaultSpawn
+
+            tp = null
         }
     }
 
     fun getCurrentSpawn(tp: S08PacketPlayerPosLook): BlockPos? {
-        if (mc.theWorld.getBlockState(BlockPos(tp.x, tp.y, tp.z).down()).block ==
-            Blocks.stonebrick && mc.theWorld.getBlockState(BlockPos(tp.x, tp.y, tp.z).down()).getValue(BlockStoneBrick.VARIANT) ==
-            BlockStoneBrick.EnumType.CHISELED) {
+        val tpPos = BlockPos(tp.x, tp.y, tp.z)
+        val state = mc.theWorld.getBlockState(tpPos.down())
+
+        if (!mc.theWorld.isBlockLoaded(tpPos.down()))
+            return null
+        else if (state.block == Blocks.stonebrick &&
+            state.getValue(BlockStoneBrick.VARIANT) == BlockStoneBrick.EnumType.CHISELED)
             return BlockPos(tp.x, tp.y, tp.z)
-        }
+
         return null
     }
 }
